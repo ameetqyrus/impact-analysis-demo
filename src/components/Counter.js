04e201a251743dbdd74b19af830831eb2f59b1ad
ameetqyrus <ameetd@outlook.com>
2024-06-26 23:42:52 +0530

    added counter changes

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -6,7 +6,7 @@ function Counter() {
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 2)}>Increment</button>
+      <button onClick={() => setCount(count + 3)}>Increment</button>
       <button onClick={() => setCount(count - 2)}>Decrement</button>
     </div>
   );
